80de997ee4c24f746d89c334db739ffacef4b51f
joox <Yousef.Ibrahim.2230238@CIT.hnu.edu.eg>
2025-07-08 19:39:09 +0300

    Update Day1.py

--- week1/Day1.py
+++ week1/Day1.py
@@ -1,32 +1,31 @@
 # the input python function
 
-# name = input("enter ur name : ")
-# print("hi"+ "  "+ name)
-
-# print("hello  "  + input("hwat is ur name  ?   " )+ "!")
+ name = input("enter ur name : ")
+ print("hi"+ "  "+ name)
+ print("hello  "  + input("hwat is ur name  ?   " )+ "!")
 #====================================================================================================
 # python variabels
 
-# name = "تخخخخء"
-# print(len(name))
+ name = "تخخخخء"
+ print(len(name))
 
-# print(len(input("What is ur name ")))
+ print(len(input("What is ur name ")))
 
 #-----------------------------------------------------------------------------------------------------
 
-# username = input("enter ur name ? ")
-# lenght = len(username)
-# print("length for this name "+ username ,  " = "+lenght) 
+ username = input("enter ur name ? ")
+ lenght = len(username)
+ print("length for this name "+ username ,  " = "+lenght) 
 
 #------------------------------------------------------------------------------------------------------
-# glass1 = "joox"
-# glass2 = "yousef"
-# temp = glass2
-# glass2 = glass1
-# glass1 = temp 
+ glass1 = "joox"
+ glass2 = "yousef"
+ temp = glass2
+ glass2 = glass1
+ glass1 = temp 
 
-# print(glass1,glass2)
-# glass1, glass2 = glass2, glass1  # Python magic! ✨
+ print(glass1,glass2)
+ glass1, glass2 = glass2, glass1  # Python magic! ✨
 
 #-------------------------------------------------------------------------------------------------------
 
