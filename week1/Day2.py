96568993d81e7c7c6d66bde2ea547485c4f33b60
joox <Yousef.Ibrahim.2230238@CIT.hnu.edu.eg>
2025-07-08 21:23:10 +0300

    simple Tip Calculator

--- week1/Day2.py
+++ week1/Day2.py
@@ -1,10 +1,22 @@
-# subscripting
 
-print("helll0"[-1])
 
-# Datatype
-print("my age : " +str(111))
-print(111+999)
-print(9*4)
-print(3/2)
-print(type(9-3))
+#simple
+
+# print("welcome to the tip calculator!")
+
+# clac = int(input("what was the total bill  "))
+# tip_calc = int(input("How much tip whould like to give? "))
+# print(int(clac) / int(tip_calc))
+
+#like bro do this 
+
+print("welcome to the tip calculator")
+bill = float(input("what is ur total billl ?  $"))
+tip = int(input("what percentage tip would you like to give ? 10% 20%"))
+people = int(input("How meny pepole to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount  = round(bill_per_person, 2 )
+print (f"Eech persone should pay : $ {final_amount}")
