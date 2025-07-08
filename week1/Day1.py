3f57c4d16256d31c6421c0c18e2b024a6c9ea9be
joox <Yousef.Ibrahim.2230238@CIT.hnu.edu.eg>
2025-07-08 19:17:42 +0300

    in day 4 i learn vaeabile

--- week1/Day1.py
+++ week1/Day1.py
@@ -1,1 +1,28 @@
-print("hello")
+# the input python function
+
+# name = input("enter ur name : ")
+# print("hi"+ "  "+ name)
+
+# print("hello  "  + input("hwat is ur name  ?   " )+ "!")
+#====================================================================================================
+# python variabels
+
+# name = "تخخخخء"
+# print(len(name))
+
+# print(len(input("What is ur name ")))
+
+#-----------------------------------------------------------------------------------------------------
+
+# username = input("enter ur name ? ")
+# lenght = len(username)
+# print("length for this name "+ username ,  " = "+lenght) 
+
+#------------------------------------------------------------------------------------------------------
+glass1 = "joox"
+glass2 = "yousef"
+temp = glass2
+glass2 = glass1
+glass1 = temp 
+
+print(glass1,glass2)
