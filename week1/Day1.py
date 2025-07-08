9604287f624975943832818b48e472c1ff9b1153
joox <Yousef.Ibrahim.2230238@CIT.hnu.edu.eg>
2025-07-08 20:44:54 +0300

    some Mathmatical Opration

--- week1/Day1.py
+++ week1/Day1.py
@@ -1,32 +1,32 @@
 # the input python function
 
-# name = input("enter ur name : ")
-# print("hi"+ "  "+ name)
+name = input("enter ur name : ")
+print("hi"+ "  "+ name)
 
-# print("hello  "  + input("hwat is ur name  ?   " )+ "!")
+print("hello  "  + input("hwat is ur name  ?   " )+ "!")
 #====================================================================================================
 # python variabels
 
-# name = "تخخخخء"
-# print(len(name))
+name = "تخخخخء"
+print(len(name))
 
-# print(len(input("What is ur name ")))
+print(len(input("What is ur name ")))
 
 #-----------------------------------------------------------------------------------------------------
 
-# username = input("enter ur name ? ")
-# lenght = len(username)
-# print("length for this name "+ username ,  " = "+lenght) 
+username = input("enter ur name ? ")
+lenght = len(username)
+print("length for this name "+ username ,  " = "+lenght) 
 
 #------------------------------------------------------------------------------------------------------
-# glass1 = "joox"
-# glass2 = "yousef"
-# temp = glass2
-# glass2 = glass1
-# glass1 = temp 
+glass1 = "joox"
+glass2 = "yousef"
+temp = glass2
+glass2 = glass1
+glass1 = temp 
 
-# print(glass1,glass2)
-# glass1, glass2 = glass2, glass1  # Python magic! ✨
+print(glass1,glass2)
+glass1, glass2 = glass2, glass1  # Python magic! ✨
 
 #-------------------------------------------------------------------------------------------------------
 
@@ -34,7 +34,7 @@
 print("welcome to the Band name generator")
 city_name  = input("witch city did you grow up in ")
 pet = input("what is the name of a pet? ")
-print("ur band anme could be :"  + city_name +" "+ pet)
+print("ur band anme could be: "  + city_name +" "+ pet)
 
 
 
---
+++ week1/Day2.py
@@ -0,0 +1,10 @@
+# subscripting
+
+print("helll0"[-1])
+
+# Datatype
+print("my age : " +str(111))
+print(111+999)
+print(9*4)
+print(3/2)
+print(type(9-3))
