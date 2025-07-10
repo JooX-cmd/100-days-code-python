9b5e812e724caaa55b4845ee3c4fb9badfa3e75d
joox <Yousef.Ibrahim.2230238@CIT.hnu.edu.eg>
2025-07-10 11:08:01 +0300

    Rock , Paper, scissor Game

--- week1/Day4.py
+++ week1/Day4.py
@@ -18,10 +18,105 @@ import random
 
 
 
-friends = ["lalal", "111efefwef", "jpkrpjgr"]
+# friends = ["lalal", "111efefwef", "jpkrpjgr"]
 
-print(random.choice(friends))
+# print(random.choice(friends))
 
 # sec option 
-random_index = random.randint(0,2)
-print(friends[random_index])
+# random_index = random.randint(0,2)
+# print(friends[random_index])
+#-----------------------------------------------------------------------------------------------------------
+
+# frist try  - my self code 
+
+# Rock , Paper, scissor Game
+
+Rock = ''' 
+
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+'''
+
+Paper = ''' 
+
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+'''
+
+scissor = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+'''
+# win 
+# lose
+# t3adol
+
+choice  =[Rock,Paper,scissor]
+choice_name = ['''Rock,Paper,scissor''']
+print("choice ")
+print("Type 0 for Rock, 1 for Paper, 2 for Scissors")
+user_choice = int(input("Enter ur choice"))
+if user_choice < 0 or user_choice >2 :
+       print("Invalid choice! You lose by default 😅")
+else:
+    
+    
+    print(choice[user_choice])
+    computer_choice = random.randint(0 ,2 )
+
+
+    print(choice[computer_choice])
+    
+    if user_choice == computer_choice:
+        print("🤝 It's a tie! ")
+    elif (user_choice == 0 and computer_choice == 2) or \
+         (user_choice == 1 and computer_choice == 0) or \
+         (user_choice == 2 and computer_choice == 1):
+        print("🎉 You win! ")
+    else:
+        print("💻 Computer wins! !")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
